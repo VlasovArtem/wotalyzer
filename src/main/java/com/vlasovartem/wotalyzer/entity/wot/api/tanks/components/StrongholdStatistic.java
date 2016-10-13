326f41c0a301f0e9ffec667716affeb93b3b3d86
Vlasov Artem <avlasov@selectica.com>
2016-10-14 00:38:16 +0300

    Statistic object refactoring

--- src/main/java/com/vlasovartem/wotalyzer/entity/wot/api/tanks/components/StrongholdStatistic.java
+++ src/main/java/com/vlasovartem/wotalyzer/entity/wot/api/tanks/components/StrongholdStatistic.java
@@ -1,78 +1,40 @@
 package com.vlasovartem.wotalyzer.entity.wot.api.tanks.components;
 
+import com.vlasovartem.wotalyzer.entity.wot.api.account.player.statistics.BasicStatistics;
+
 /**
  * Created by artemvlasov on 14/10/2016.
  */
-public class StrongholdStatistic extends ClanStatistic {
-    
-    /* Direct hits received */
-    private int directHitsReceived;
-    /* Hits on enemy as a result of splash damage */
-    private int explosionHits;
-    /* Hits received as a result of splash damage */
-    private int explosionHitsReceived;
-    /* Direct hits received that caused no damage */
-    private int noDamageDirectHitsReceived;
-    /* Penetrations */
-    private int piercings;
-    /* Penetrations received */
-    private int piercingsReceived;
-    /* Ratio of damage blocked by armor from AP, HEAT, and APCR shells to damage received from these types of shells. Value is calculated starting from version 9.0. */
-    private float tankingFactor;
+public class StrongholdStatistic extends BasicStatistics {
 
-    public int getDirectHitsReceived() {
-        return directHitsReceived;
+    /* Maximum Damage caused per battle */
+    private int maxDamage;
+    /* Maximum destroyed in battle */
+    private int maxFrags;
+    /* Maximum experience per battle */
+    private int maxXp;
+
+    public int getMaxDamage() {
+        return maxDamage;
     }
 
-    public void setDirectHitsReceived(int directHitsReceived) {
-        this.directHitsReceived = directHitsReceived;
+    public void setMaxDamage(int maxDamage) {
+        this.maxDamage = maxDamage;
     }
 
-    public int getExplosionHits() {
-        return explosionHits;
+    public int getMaxFrags() {
+        return maxFrags;
     }
 
-    public void setExplosionHits(int explosionHits) {
-        this.explosionHits = explosionHits;
+    public void setMaxFrags(int maxFrags) {
+        this.maxFrags = maxFrags;
     }
 
-    public int getExplosionHitsReceived() {
-        return explosionHitsReceived;
+    public int getMaxXp() {
+        return maxXp;
     }
 
-    public void setExplosionHitsReceived(int explosionHitsReceived) {
-        this.explosionHitsReceived = explosionHitsReceived;
-    }
-
-    public int getNoDamageDirectHitsReceived() {
-        return noDamageDirectHitsReceived;
-    }
-
-    public void setNoDamageDirectHitsReceived(int noDamageDirectHitsReceived) {
-        this.noDamageDirectHitsReceived = noDamageDirectHitsReceived;
-    }
-
-    public int getPiercings() {
-        return piercings;
-    }
-
-    public void setPiercings(int piercings) {
-        this.piercings = piercings;
-    }
-
-    public int getPiercingsReceived() {
-        return piercingsReceived;
-    }
-
-    public void setPiercingsReceived(int piercingsReceived) {
-        this.piercingsReceived = piercingsReceived;
-    }
-
-    public float getTankingFactor() {
-        return tankingFactor;
-    }
-
-    public void setTankingFactor(float tankingFactor) {
-        this.tankingFactor = tankingFactor;
+    public void setMaxXp(int maxXp) {
+        this.maxXp = maxXp;
     }
 }
