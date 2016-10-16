aaa81adfa996126e32317fc701c6f52e5ae59345
Vlasov Artem <avlasov@selectica.com>
2016-10-16 13:21:06 +0300

    Add Season Rating entity, constants and utils

---
+++ src/main/java/com/vlasovartem/wotalyzer/utils/api/contstans/global_map/SeasonRatingConstants.java
@@ -0,0 +1,19 @@
+package com.vlasovartem.wotalyzer.utils.api.contstans.global_map;
+
+import com.vlasovartem.wotalyzer.utils.api.contstans.BasicAPIConstants;
+import com.vlasovartem.wotalyzer.utils.api.contstans.utils.ConstantUtils;
+
+import java.util.List;
+
+import static com.vlasovartem.wotalyzer.utils.api.contstans.WOTAPIConstants.*;
+
+/**
+ * Created by artemvlasov on 16/10/2016.
+ */
+public interface SeasonRatingConstants extends BasicAPIConstants {
+
+    String BASIC_URL = "https://developers.wargaming.net/reference/all/wot/globalmap/seasonrating";
+    List<String> BASIC_API_CONSTANTS = ConstantUtils.generate(BasicAPIConstants.BASIC_API_CONSTANTS, SEASON_ID_PARAM, VEHICLE_LEVEL_PARAM, LIMIT_PARAM, PAGE_NO_PARAM);
+    List<String> REQUIRED_PARAMS = ConstantUtils.generate(BasicAPIConstants.REQUIRED_PARAMS, SEASON_ID_PARAM, VEHICLE_LEVEL_PARAM);
+
+}
